02563b08ed18ee7743e167e9749a84fd6871694a
Akram3004 <akram1316709@gmail.com>
2024-10-03 10:36:56 +0530

    4 comm

--- pages/api/auth/google.js
+++ pages/api/auth/google.js
@@ -1,12 +1,14 @@
 import { google } from 'googleapis';
 
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI
-);
-
 export default async function handler(req, res) {
+  const redirectUri = process.env.GOOGLE_REDIRECT_URI;
+
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    redirectUri
+  );
+
   if (req.method === 'GET') {
     const authUrl = oauth2Client.generateAuthUrl({
       access_type: 'offline',
